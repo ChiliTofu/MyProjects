72f91265fccda962b5fd0b93d9fc5533b10ad6d3
ChiliaTofu <89005745+ChiliTofu@users.noreply.github.com>
2021-09-08 18:46:58 +0200

    Thunderbolt

---
+++ TriggerInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Klass för att hålla metoder vi kan köra från en Unity UI knapp
+public class TriggerInteraction : MonoBehaviour
+{
+    //Metoden vi vill köra från vår UI knapp.
+    public void TriggerInteractionOnClick()
+    {
+        //Kolla så att vi har en Entity markerad
+        if (Selection.Selected != null)
+        {
+            //Skriv ett meddelande till konsolen om vilken Entity vi har Triggat
+            Debug.Log(Selection.Selected.EntityName + " is sooo triggered!");
+        }
+        else //Annars
+        {
+            //Meddela oss att vi inte har någon Entity markerad
+            Debug.Log("Nothing is selected, therefore nothing to trigger");
+        }
+    }
+}
